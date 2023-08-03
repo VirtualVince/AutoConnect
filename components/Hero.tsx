14a572dae1b9ace37742bd3c8622c230ba532b4a
VirtualVince <virtualvince2020@gmail.com>
2023-08-03 12:56:00 -0400

    navbar footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,6 +16,7 @@ const Hero = () => {
         </p>
         <CustomButton
           title="Explore Cars"
+          btnType="button"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handlescroll}
         />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "AutoConnect",
-  description: "Created by Virtual Vince",
+  description:
+    "Streamline your car rental experience. Created by Virtual Vince",
 };
 
 export default function RootLayout({
@@ -16,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={"relative"}>{children}</body>
     </html>
   );
 }
